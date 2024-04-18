b87c4b4375e7d8c12e0a5e85b3f28cc0ace14937
atto08 <atto08@naver.com>
2024-04-18 16:59:03 +0900

    add solve introduce algorithm

--- src/programmers/Carpet.java
+++ src/programmers/Carpet.java
@@ -1,8 +1,15 @@
 package programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/*
+카펫 - level2
+소요 시간: 42분
+1) 통과
+>> 타일의 총합의 약수들 중 가로길이가 가장 작을 때 부터 검사하는 방식으로 구현
+ */
 public class Carpet {
     static int w, h;
     public int[] solution(int brown, int yellow) {
@@ -49,5 +56,12 @@ public class Carpet {
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet();
+        System.out.println(Arrays.toString(carpet.solution(10,2)));
+        System.out.println(Arrays.toString(carpet.solution(8,1)));
+        System.out.println(Arrays.toString(carpet.solution(24,24)));
     }
 }
