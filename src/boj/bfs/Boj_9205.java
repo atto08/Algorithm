bfd46b4008b3b752d25be1cfce6c92c13face76d
atto08 <atto08@naver.com>
2025-06-23 15:33:30 +0900

    solve 맥주마시면서 걸어가기(G5) - [bfs Algorithm]

--- src/boj/bfs/Boj_9205.java
+++ src/boj/bfs/Boj_9205.java
@@ -35,7 +35,6 @@ public class Boj_9205 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
 
         for (int test_case = 1; test_case <= T; test_case++) {
