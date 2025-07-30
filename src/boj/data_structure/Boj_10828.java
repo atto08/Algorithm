35f2a36707c5951c0f1eba9d8ad9e5dc16248175
atto08 <atto08@naver.com>
2025-07-30 11:01:19 +0900

    2회차 solve 스택(S4) - [implement Algorithm]

--- src/boj/data_structure/Boj_10828.java
+++ src/boj/data_structure/Boj_10828.java
@@ -1,5 +1,21 @@
 package boj.data_structure;
 
+/*
+스택 - 실4
+소요시간 - 22분
+
+2회차 풀이
+문제설명:
+push X: 정수 X를 스택에 넣는 연산
+pop : 스택에서 가장위에있는 정수 제거 + 출력 (정수가 없다면 -1출력)
+size: 스택크기 출력
+empty: 스택비어있으면 1 아님 0 출력
+top: 스택 가장 위에 있는 정수 출력 --> peek (정수가 없다면 -1출력)
+
+p.s
+- 똑같이 풀었다 신기하다. 그래서 코드는 따로 첨부하지 않겠다.
+- 더 빠르고 쉬운 방법을 생각해봐야겠다.
+ */
 import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
