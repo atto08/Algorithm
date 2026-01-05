6992f46311fbb149a7ecc126dd5f33c72b9f2772
atto08 <atto08@naver.com>
2026-01-06 03:18:08 +0900

    2번째 풀이 (s1) - [greedy Algorithm]
    - 시간초과
    - stack.contains() -> 내부적으로 선형 탐색(O(stack size))
    •	while이 N번 돌고
    •	매번 contains가 평균 O(N)
    결과적으로 O(N²) 발생

--- src/boj/greedy/Boj_1946.java
+++ src/boj/greedy/Boj_1946.java
@@ -11,34 +11,31 @@ public class Boj_1946 {
         StringBuilder result = new StringBuilder();
         // 테스트케이스 수 만큼
         while (T > 0) {
-            List<User> pos = new ArrayList<>();
             int N = Integer.parseInt(br.readLine());
+
+            User[] pos = new User[N];
             int[] rank1 = new int[N];   // 서류 등수
             int[] rank2 = new int[N];   // 면접 등수
             for (int num = 0; num < N; num++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int i1 = Integer.parseInt(st.nextToken()) - 1;
-                int i2 = Integer.parseInt(st.nextToken()) - 1;
-                pos.add(new User(i1, i2));
-
-                rank1[i1] = num;
-                rank2[i2] = num;
+                int r1 = Integer.parseInt(st.nextToken()) - 1;
+                int r2 = Integer.parseInt(st.nextToken()) - 1;
+                pos[num] = new User(r1, r2);
+                rank1[r1] = num;
+                rank2[r2] = num;
             }
 
-            // 1등부터 N등까지 확인하기
-            boolean[] visited = new boolean[N];
-            int max = 0;
-            for (int i = 0; i < N; i++) {
-                int r1 = rank1[i];
-                if (visited[r1]) continue;   // 이미 탈락임
-                //
-                max++;
-                // 현재 서류등수보다 낮은등수 지원자들 번호 방문처리
-                for (int j = i + 1; j < N; j++) visited[rank1[j]] = true;
-                // 현재 서류 상위인 녀석의 면접 등수
-                int r2 = pos.get(r1).r2;
-                // 현재 지원자 보다 면접 등수가 높은 애들은 다시 살리기
-                for (int j = r2 - 1; j >= 0; j--) visited[rank2[j]] = false;
+            Stack<Integer> stack = new Stack<>();
+            for (int n : rank1) stack.push(n);
+            int max = N;
+
+            while (!stack.isEmpty()) {
+                int n = stack.pop();
+                int r2 = pos[n].r2;
+                if (r2 == 0) continue;
+
+                int target = rank2[r2 - 1];
+                if (stack.contains(target)) max--;
             }
             result.append(max).append("\n");
             T--;
